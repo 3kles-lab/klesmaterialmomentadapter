166919b4124a6f72f50afa19ff5de459502820e9
Quentin Tellier <QTELLIER@3KLES.local>
2025-04-08 10:19:43 +0200

    Update angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { FormGroup, FormControl } from '@angular/forms';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    providers: []
+    providers: [],
+    standalone: false
 })
 export class AppComponent {
     form: FormGroup;
